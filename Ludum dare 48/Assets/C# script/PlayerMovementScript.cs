c4b7d3d66157c449ae5292819fa80c21a3eb6761
Fireye <codekai16@gmail.com>
2021-04-24 16:54:59 -0700

    player controller

---
+++ Ludum dare 48/Assets/C# script/PlayerMovementScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+    bool jump = false;
+
+    float xMove = 0f;
+    float yMove = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        xMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
+    }
+
+    void FixedUpdate() {
+
+        controller.Move(xMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
