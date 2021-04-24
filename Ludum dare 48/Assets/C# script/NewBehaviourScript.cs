6d2a684708b89ae7806ec4f78702642ddf95af9a
vkedia05 <vvkedia007@gmail.com>
2021-04-23 21:26:52 -0700

    IDK

---
+++ Ludum dare 48/Assets/C# script/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    private book movingRight = true;
+    public Transform groundDetetction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
